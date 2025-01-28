66e49ad6004f7933efb0b5654833dc76ef9ed75c
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-28 15:06:32 -0600

    Working k8s (no act_runner) (#1)
    * Initial steps towards k8s
    
    * Working initialize script

--- gitea/initialize-gitea.sh
+++ gitea/initialize-gitea.sh
@@ -15,34 +15,27 @@ secure_password() {
 GITEA_USER=${BPI_GITEA_NEW_ADMIN_USERNAME:-"gitea_admin"}
 GITEA_PASSWORD=${BPI_GITEA_SET_NEW_ADMIN_PASSWORD:-"$(secure_password)"}
 GITEA_USER_EMAIL=${BPI_GITEA_SET_NEW_ADMIN_EMAIL:-${GITEA_USER}@example.com}
-GITEA_NEW_ORGANIZATION=${BPI_GITEA_NEW_ORGANIZATION:-"cerc-io"}
+GITEA_NEW_ORGANIZATION=${BPI_GITEA_NEW_ORGANIZATION:-"bpi"}
 GITEA_URL_PREFIX=http://localhost:3000
-BPI_GITEA_TOKEN_NAME=laconic-so-publication-token
+BPI_GITEA_TOKEN_NAME=stack-publication-token
 
 if ! [[ -n "$BPI_GITEA_RUNNER_REGISTRATION_TOKEN" ]]; then
     echo "Warning: using insecure default runner registration token"
     BPI_GITEA_RUNNER_REGISTRATION_TOKEN=eMdEwIzSo87nBh0UFWZlbp308j6TNWr3WhWxQqIc
 fi
 
-# Create admin user
-# First check if it already exists
-if [[ -z ${BPI_SO_COMPOSE_PROJECT} ]] ; then
-    compose_command="docker compose"
-else
-    compose_command="docker compose -p ${BPI_SO_COMPOSE_PROJECT}"
-fi
 sleep 15
-${compose_command} exec --user git server gitea admin user list --admin | grep -v -e "^ID" | awk '{ print $2 }' | grep ${GITEA_USER} > /dev/null
+EXEC_CMD="stack deployment --dir ${BPI_SO_DEPLOYMENT_DIR} exec gitea"
+EXEC_CMD_DB="stack deployment --dir ${BPI_SO_DEPLOYMENT_DIR} exec db"
+${EXEC_CMD} "su -c 'gitea admin user list --admin' git" | grep -v -e '^ID' | awk '{ print $2 }' | grep ${GITEA_USER} > /dev/null
 if [[ $? == 1 ]] ; then
     # Then create if it wasn't found
-    ${compose_command} exec --user git server gitea admin user create --admin --username ${GITEA_USER} --password ${GITEA_PASSWORD} --email ${GITEA_USER_EMAIL}
+    ${EXEC_CMD} "su -c 'gitea admin user create --admin --username ${GITEA_USER} --password ${GITEA_PASSWORD} --email ${GITEA_USER_EMAIL}' git"
 fi
 # HACK: sleep a bit because if we don't gitea will return empty responses
 sleep 5
 # Check if the token already exists
-token_response=$( curl -s "${GITEA_URL_PREFIX}/api/v1/users/${GITEA_USER}/tokens" \
-  -u ${GITEA_USER}:${GITEA_PASSWORD} \
-  -H "Content-Type: application/json")
+token_response=$(${EXEC_CMD} "curl -s '${GITEA_URL_PREFIX}/api/v1/users/${GITEA_USER}/tokens' -u '${GITEA_USER}:${GITEA_PASSWORD}' -H 'Content-Type: application/json'")
 if [[ -n ${token_response} ]] ; then
     # Simple check for re-running this script. Ideally we should behave more elegantly.
     if [[ "${token_response}" == *"password is invalid"* ]]; then
@@ -59,13 +52,10 @@ if [[ ${token_found} != 1 ]] ; then
     # Note that we either create the token here, or we needed to be passed 
     # the token by the caller. This is because gitea won't release the token
     # plaintext post-creation.
-    new_gitea_token=$( curl -s -X POST "${GITEA_URL_PREFIX}/api/v1/users/${GITEA_USER}/tokens" \
-      -u ${GITEA_USER}:${GITEA_PASSWORD} \
-      -H "Content-Type: application/json" \
-      -d '{"name":"'${BPI_GITEA_TOKEN_NAME}'", "scopes": [ "read:admin", "write:admin", "read:organization", "write:organization", "read:repository", "write:repository", "read:package", "write:package" ] }' \
-      | jq -r .sha1 )
+    new_gitea_token_response=$( $EXEC_CMD "curl -s '${GITEA_URL_PREFIX}/api/v1/users/${GITEA_USER}/tokens' -u '${GITEA_USER}:${GITEA_PASSWORD}' -H  'accept: application/json' -H 'Content-Type: application/json' -d '{\"name\":\"'${BPI_GITEA_TOKEN_NAME}'\", \"scopes\": [ \"read:admin\", \"write:admin\", \"read:organization\", \"write:organization\", \"read:repository\", \"write:repository\", \"read:package\", \"write:package\" ] }'" )
+    new_gitea_token=$(echo $new_gitea_token_response | jq -r '.["sha1"]')
     echo "NOTE: This is your gitea access token: ${new_gitea_token}. Keep it safe and secure, it can not be fetched again from gitea."
-    echo "NOTE: To use with laconic-so set this environment variable: export BPI_NPM_AUTH_TOKEN=${new_gitea_token}"
+    echo "NOTE: To use with stack set this environment variable: export BPI_NPM_AUTH_TOKEN=${new_gitea_token}"
     BPI_GITEA_AUTH_TOKEN=${new_gitea_token}
 else
     # If the token exists, then we must have been passed its value.
@@ -79,26 +69,15 @@ fi
 # we can proceed with token-authenticated API requests below
 # Create org
 # First check if it already exists
-curl -s "${GITEA_URL_PREFIX}/api/v1/admin/users/${GITEA_USER}/orgs" \
-  -H "Authorization: token ${BPI_GITEA_AUTH_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -H  "accept: application/json" \
-  | jq --exit-status -r 'to_entries[] | select(.value.name == "'${GITEA_NEW_ORGANIZATION}'")' > /dev/null
-if [[ $? != 0 ]] ; then
+${EXEC_CMD} "curl -s '${GITEA_URL_PREFIX}/api/v1/admin/users/${GITEA_USER}/orgs' -H 'Authorization: token ${BPI_GITEA_AUTH_TOKEN}' -H 'Content-Type: application/json' -H  'accept: application/json'" | jq --exit-status -r 'to_entries[] | select(.value.name == "'${GITEA_NEW_ORGANIZATION}'")' > /dev/null
+if [[ $? != 0 ]]; then
     # If it doesn't exist, create it
     # See: https://discourse.gitea.io/t/create-remove-organization-through-api/478
-    curl -s -X POST "${GITEA_URL_PREFIX}/api/v1/admin/users/${GITEA_USER}/orgs" \
-      -H "Authorization: token ${BPI_GITEA_AUTH_TOKEN}" \
-      -H "Content-Type: application/json" \
-      -H  "accept: application/json" \
-      -d '{"username": "'${GITEA_NEW_ORGANIZATION}'"}' > /dev/null
-    echo "Created the organization ${GITEA_NEW_ORGANIZATION}"
+    ${EXEC_CMD} "curl -s -X POST '${GITEA_URL_PREFIX}/api/v1/admin/users/${GITEA_USER}/orgs' -H 'Authorization: token ${BPI_GITEA_AUTH_TOKEN}' -H 'Content-Type: application/json' -H  'accept: application/json' -d '{\"username\": \"${GITEA_NEW_ORGANIZATION}\"}'" && echo "Created the organization ${GITEA_NEW_ORGANIZATION}" || echo "Error creating organization ${GITEA_NEW_ORGANIZATION}"
 fi
 
-
 # Seed a token for act_runner registration.
-${compose_command} exec db \
-    psql -U gitea -d gitea -c "INSERT INTO public.action_runner_token(token, owner_id, repo_id, is_active, created, updated, deleted) VALUES('${BPI_GITEA_RUNNER_REGISTRATION_TOKEN}', 0, 0, 't', 1679000000, 1679000000, NULL);" >/dev/null
+${EXEC_CMD_DB} "psql -U gitea -d gitea -c \"INSERT INTO public.action_runner_token(token, owner_id, repo_id, is_active, created, updated, deleted) VALUES('${BPI_GITEA_RUNNER_REGISTRATION_TOKEN}', 0, 0, 't', 1679000000, 1679000000, NULL);\""
 
 echo "NOTE: Gitea was configured to use host name: gitea.local, ensure that this resolves to localhost, e.g. with sudo vi /etc/hosts"
 if ! [[ -n "$BPI_GITEA_SET_NEW_ADMIN_PASSWORD" ]]; then
---
+++ scripts/machine_k8s_deploy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+IMAGE_REGISTRY=""
+IMAGE_REGISTRY_USERNAME=""
+IMAGE_REGISTRY_PASSWORD=""
+HTTP_PROXY_FQDN="${BPI_MACHINE_FQDN}"
+HTTP_PROXY_CLUSTER_ISSUER=""
+
+while (( "$#" )); do
+   case $1 in
+      --image-registry)
+         shift&&IMAGE_REGISTRY="$1"||die
+         ;;
+      --image-registry-username)
+         shift&&IMAGE_REGISTRY_USERNAME="$1"||die
+         ;;
+      --image-registry-password)
+         shift&&IMAGE_REGISTRY_PASSWORD="$1"||die
+         ;;
+      --http-proxy-fqdn)
+         shift&&HTTP_PROXY_FQDN="$1"||die
+         ;;
+      --http-proxy-cluster-issuer)
+         shift&&HTTP_PROXY_CLUSTER_ISSUER="$1"||die
+         ;;
+         *)
+         echo "Unrecognized argument: $1" 1>&2
+         ;;
+   esac
+   shift
+done
+
+if [[ -z "$IMAGE_REGISTRY" ]]; then
+  if [[ -f "/etc/rancher/k3s/registries.yaml" ]]; then
+    IMAGE_REGISTRY=$(cat /etc/rancher/k3s/registries.yaml | grep -A1 'configs:$' | tail -1 | awk '{ print $1 }' | cut -d':' -f1)
+    IMAGE_REGISTRY_USERNAME=$(cat /etc/rancher/k3s/registries.yaml | grep 'username:' | awk '{ print $2 }' | sed "s/[\"']//g")
+    IMAGE_REGISTRY_PASSWORD=$(cat /etc/rancher/k3s/registries.yaml | grep 'password:' | awk '{ print $2 }' | sed "s/[\"']//g")
+  fi
+fi
+
+stack fetch-stack telackey/gitea-stack@telackey/k8s
+
+stack --stack ~/bpi/gitea-stack/stacks/gitea setup-repositories
+stack --stack ~/bpi/gitea-stack/stacks/gitea build-containers
+
+sudo chmod a+r /etc/rancher/k3s/k3s.yaml
+
+HTTP_PROXY_ARG=""
+if [[ -n "${HTTP_PROXY_FQDN}" ]]; then
+  if [[ -n "${HTTP_PROXY_CLUSTER_ISSUER}" ]]; then
+    HTTP_PROXY_ARG="--http-proxy ${HTTP_PROXY_CLUSTER_ISSUER}~${HTTP_PROXY_FQDN}:gitea:3000"
+  else
+    HTTP_PROXY_ARG="--http-proxy ${HTTP_PROXY_FQDN}:gitea:3000"
+  fi
+fi
+
+stack --stack ~/bpi/gitea-stack/stacks/gitea deploy \
+ --deploy-to k8s init --output gitea.yml \
+ --kube-config /etc/rancher/k3s/k3s.yaml \
+ --image-registry $IMAGE_REGISTRY/bozemanpass ${HTTP_PROXY_ARG}
+
+mkdir $HOME/deployments
+
+stack --stack ~/bpi/gitea-stack/stacks/gitea deploy \
+   create --spec-file gitea.yml --deployment-dir $HOME/deployments/gitea
+
+docker login --username "$IMAGE_REGISTRY_USERNAME" --password "$IMAGE_REGISTRY_PASSWORD" $IMAGE_REGISTRY
+
+stack deployment --dir $HOME/deployments/gitea push-images
+stack deployment --dir $HOME/deployments/gitea start
