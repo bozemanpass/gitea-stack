6f7b54b51b4a8bb60369a8db871dd1d50140d707
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-29 20:44:39 -0600

    dind

--- container-build/bpi-act-runner/build.sh
+++ container-build/bpi-act-runner/build.sh
@@ -2,4 +2,4 @@
 source ${BPI_CONTAINER_BASE_DIR}/build-base.sh
 # Build a local version of the act-runner image
 # TODO: enhance the default build code path to cope with this container (repo has an _ which needs to be converted to - in the image tag)
-docker build -t bpi/act-runner:local -f ${BPI_REPO_BASE_DIR}/act_runner/Dockerfile ${build_command_args} ${BPI_REPO_BASE_DIR}/act_runner
+docker build --target dind -t bpi/act-runner:local -f ${BPI_REPO_BASE_DIR}/act_runner/Dockerfile ${build_command_args} ${BPI_REPO_BASE_DIR}/act_runner
